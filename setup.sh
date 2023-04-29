4593c9f343b841a26108a8424842fea158eb55b3
Vrinda Sheena Anil <vrinda@Vrindas-MacBook-Air.local>
2023-04-29 12:37:42 -0600

    changed env name

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash 
 
-mkdir cryo_env 
-python3 -m venv cryo_env
-source cryo_env/bin/activate
+mkdir fib_env 
+python3 -m venv fib_env
+source fib_env/bin/activate
 pip3 install jupyter notebook
 jupyter notebook 
